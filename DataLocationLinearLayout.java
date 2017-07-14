bedde906a20ab4037a5d11b1cd53be0e87d3e11f
manhtienptit <manhtiend09cn2.ptit@gmail.com>
2017-07-14 12:54:39 +0700

    dfs
    sdfsf

---
+++ DataLocationLinearLayout.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2015-present Amberfog
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.seatgeek.placesautocompletedemo;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class DataLocationLinearLayout extends RelativeLayout {
+
+    public DataLocationLinearLayout(Context context) {
+        super(context);
+    }
+
+    public DataLocationLinearLayout(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DataLocationLinearLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public DataLocationLinearLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
